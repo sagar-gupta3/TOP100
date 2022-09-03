95f7174aac43855b6262c9d0126b78572f37907f
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-09-03 07:06:34 +0530

    Add files via upload

---
+++ Greatest of the Two Numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[])
+    {
+        int num1 = 12, num2 = 21;
+        if (num1 == num2)
+            System.out.println("both are equal");
+        else
+            // prints the maximum of two numbers
+            System.out.println(Math.max(num1, num2) + " is greater");
+    }
+}
