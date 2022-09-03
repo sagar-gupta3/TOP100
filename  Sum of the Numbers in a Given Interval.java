8e9ead803c98fe13c254c3aa74eaba95edcc710e
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-09-03 07:04:59 +0530

    Add files via upload

---
+++  Sum of the Numbers in a Given Interval.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int num1 = 2;
+	    int num2 = 5;
+	    int sum = num2*(num2+1)/2 - num1*(num1+1)/2 + num1;
+		System.out.println("The Sum is "+ sum);
+	}
+}
