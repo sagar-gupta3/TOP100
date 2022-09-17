c9e57667e3d71a8cd1b2aed3687ffcf66cbeabc2
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-09-17 15:11:49 +0530

    Add files via upload

---
+++ PRIME NUMBER BW 1 TO 100.java
@@ -0,0 +1,32 @@
+//Prime Number between 1 to 100
+public class Main
+{   
+    
+	public static void main(String[] args) {
+		int a=1,b=100;
+		for(int i=a;i<=b;i++){
+		    if(checkPrime(i)){
+		        System.out.print(i+" " );
+		    }
+		}
+	}
+	public static boolean checkPrime(int num){
+	    
+	    if(num<2){
+	        return false;
+	    }
+	    else{
+	        
+	 // no need to run loop till num-1 as for any number x the numbers in
+    // the range(num/2 + 1, num) won't be divisible anyways. 
+    // Example 36 wont be divisible by anything b/w 19-35
+	        int x= num/2;
+	        for(int i=2;i<x;i++){
+	            if(num%i==0){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+}
