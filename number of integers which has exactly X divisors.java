14bbab92e5a9cbe34ec68e3717e0e9ab87823041
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-09-18 17:12:10 +0530

    Add files via upload

---
+++ number of integers which has exactly X divisors.java
@@ -0,0 +1,25 @@
+//Write a program to count Number of integers which has exactly X divisors using Java
+import java.util.*;
+ 
+class Main{
+    public static void main(String[] args)
+    {
+        int  n = 7, x = 2 ;
+        int count = 0;
+    
+        for(int i=1; i<=n; i++){
+        
+            //variable to count the factors of i-th number
+            int count_factors = 0;
+            for(int j = 1; j<= i; j++){
+                if(i%j==0){
+                    count_factors++;
+                }
+            }
+        
+        if(count_factors == x)
+            count++;
+    }
+        System.out.println(count);
+    }
+}
